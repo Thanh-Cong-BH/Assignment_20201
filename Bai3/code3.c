52f7589e37398f6f726a50e5a7555ed3015f13aa
Thanh-Cong-BH <75298436+Thanh-Cong-BH@users.noreply.github.com>
2021-01-31 10:12:59 +0700

    Add files via upload

--- Bai3/code3.c
+++ Bai3/code3.c
@@ -4,97 +4,84 @@
 
 void option1()
 {
-    student member;
+    student enrolled;
 
-    FILE *input = fopen("SV2021.dat", "r");
-
-    while (fscanf(input, "%[^\t]\t%f\n", member.name, &member.score) != EOF)
-    {
-        printf("- %s\t%.2f\n", member.name, member.score);
-    }
+    FILE *input = fopen("SV2021.dat", "r+b");
     
+	while (fscanf(input, "%[^\t]\t%lf\n", enrolled.name, &enrolled.score) != EOF)
+    {
+        printf("- %s\t%.2f\n", enrolled.name, enrolled.score);
+    }
     fclose(input);
 }
 
 void option2()
 {
-    student new;
+    student added;
 
-    FILE *input = fopen("SV2021.dat", "a");
+    FILE *input = fopen("SV2021.dat", "a+b");
 
     printf("Enter name: ");
     getchar();
-    gets(new.name);
+    gets(added.name);
     printf("Enter score: ");
-    scanf("%f", &new.score);
-    fprintf(input, "%-*s\t%.2f\n", 160, new_student.name, new_student.score);
-
-    // Close SV2021.dat
-    fclose(input);
+    scanf("%f", &added.score);
+    fprintf(input, "%*s\t %.2f\n", 160, added.name, added.score);
+    
+	fclose(input);
 }
 
-void option3()
+void option3(void)
 {
-    student member_SV2021;
+    student SV2021;
     char search[160];
-    int N = 0;
+    int count = 0;
 
-    FILE *input = fopen("SV2021.dat", "r");
+    FILE *input = fopen("SV2021.dat", "r+b");
 
     printf("Enter name: ");
     getchar();
     gets(search);
 
-    while (fscanf(input, "%[^\t]\t%f\n", member_SV2021.name, &member_SV2021.score) != EOF)
+    while (fscanf(input, "%[^\t]\t%lf\n",SV2021.name, &SV2021.score) != EOF)
     {
-        if (strstr(member_SV2021.name, search) != NULL)
+        if (strstr(SV2021.name, search) != NULL)
         {
-            printf("- %-*s\t%.2f\n", 160, member_SV2021.name, member_SV2021.score);
-            N++;
+            printf("%*s\t %.2lf\n", 160,SV2021.name, SV2021.score);
+            count++;
         }
     }
-    if (N == 0)
-        printf("No result.\n");
+    if (count == 0)
+        printf("No result!\n");
 
     fclose(input);
 }
 
-int main()
+int main(void)
 {
-    int choice;
+    int option;
 
-    do
+	while (option != 4)
     {
-        printf("Chuong trinh quan ly sinh vien\n\n");
-        printf("| 1. Xem danh sach sinh vien\n");
-        printf("| 2. Nhap them sinh vien\n");
-        printf("| 3. Tim sinh vien theo ten\n");
-        printf("| 4. Thoat\n\n");
-       
-        printf("Yeu cau cua ban la: ");
-        if (scanf("%d", &choice) != 1)
-            return 1;
+        printf("\n");
+        printf("1. Xem danh sach sinh vien\n");
+        printf("2. Nhap them sinh vien\n");
+        printf("3. Tim sinh vien theo ten\n");
+        printf("4. Thoat\n");
 
-        switch (choice)
-        {
-        case 1:
-            option1();
-            break;
-        case 2:
-            option2();
-            break;
-        case 3:
-            option3();
-            break;
-        case 4:
-            return 0;
-        default:
-            return 1;
-        }
-
+        printf("Lua chon hanh dong can thuc thi: ");
+    	scanf("%d", &option);
+		if (option == 1)
+        	option1();
+        else if (option == 2)
+        	option2();
+        else if (option == 3)
+			option3();
+		else if (option == 4)
+			break;
         printf("\n");
     }
-    while (1);
+
 }
-    
-  
+
+
